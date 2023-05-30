ad40a1e6d7a9c373ef14b07c7817475112e441d0
Bookuh <131380823+Bookuh@users.noreply.github.com>
2023-05-30 15:31:06 -0500

    Add files via upload

---
+++ sql_hw.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from IPython.display import display
+from sqlalchemy.types import Integer, Text, String, DateTime, Float
+
+data = pd.read_csv(r'C:\Users\willi\OneDrive\Desktop\Data\Coding Temple Week 5\titanic (1).csv')
+# display(data.head())
+connection = "postgresql://yyesfgnz:4uoejwmoNhZNgR3b8XXVk_a9W1hjoNBR@rajje.db.elephantsql.com/yyesfgnz"
+data.to_sql('Titanic', con = connection)
+display(pd.read_sql_table('Titanic', con = connection))
