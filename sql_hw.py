ec9338bff7eb6f12396a8d607aa1adcf872d75e6
Bookuh <131380823+Bookuh@users.noreply.github.com>
2023-05-30 15:30:44 -0500

    Delete sql_hw.py

--- sql_hw.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from IPython.display import display
-from sqlalchemy.types import Integer, Text, String, DateTime, Float
-
-data = pd.read_csv(r'C:\Users\willi\OneDrive\Desktop\Data\Coding Temple Week 5\titanic (1).csv')
-# display(data.head())
-connection = "postgresql://yyesfgnz:4uoejwmoNhZNgR3b8XXVk_a9W1hjoNBR@rajje.db.elephantsql.com/yyesfgnz"
-data.to_sql('Ship', con = connection)
-display(pd.read_sql_table('Ship', con = connection))
